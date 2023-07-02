810ffdd505b93f197fa150f2f0956b5a4c1817a4
Omar Campos <omarjc-1997@hotmail.com>
2023-07-01 21:02:28 -0500

    changes in deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TopsecretModule } from './controllers/topsecret/topsecret.module';
 import { HealtcheckModule } from './controllers/healtcheck/healtcheck.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [TopsecretModule, HealtcheckModule],
+  imports: [
+    TopsecretModule,
+    HealtcheckModule,
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
+  ],
   controllers: [],
   providers: [],
 })
